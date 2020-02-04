d6e9b82f71e8d2f1ea4e990d14e2b27cc83ae004
YaoHong <Yao_h@sina.cn>
2020-02-04 11:56:22 +0800

    全局自定义样式完成

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import vant from 'vant' // 引入vant组件库
 import 'vant/lib/index.css' // 样式文件
+import '@/styles/index.less' // 引入全局自定义样式
 import 'amfe-flexible' // 引入自动配置插件
 Vue.use(vant) // 一旦完成祖册  在任意地方都可以使用vant组件库
 Vue.config.productionTip = false
