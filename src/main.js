9b4a2b1cec6c7df1b1704e4ef19d09f04062f16e
YaoHong <Yao_h@sina.cn>
2020-02-04 14:40:12 +0800

    登录布局及颜色配置

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './permission'
 import store from './store'
 import vant from 'vant' // 引入vant组件库
-import 'vant/lib/index.css' // 样式文件
+import 'vant/lib/index.less' // 样式文件
 import '@/styles/index.less' // 引入全局自定义样式
 import 'amfe-flexible' // 引入自动配置插件
 Vue.use(vant) // 一旦完成祖册  在任意地方都可以使用vant组件库
