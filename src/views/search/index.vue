48345a3171abf18f77685f08556741d43940c971
YaoHong <Yao_h@sina.cn>
2020-02-04 23:51:43 +0800

    搜索关键词 和历史记录及联想搜索

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -3,15 +3,15 @@
     <!-- 搜索组件一级路由   返回上一个页面-->
     <van-nav-bar left-arrow title="搜索中心" @click-left="$router.back()"></van-nav-bar>
     <!-- 导航 -->
-    <van-search  placeholder="请输入搜索关键词" shape="round" />
+    <van-search v-model.trim="q" placeholder="请输入搜索关键词" shape="round" />
     <!-- 联想搜索 -->
-    <van-cell-group class="suggest-box" >
+    <van-cell-group class="suggest-box" v-if="q">
       <van-cell icon="search">
         <span>j</span>ava
       </van-cell>
     </van-cell-group>
     <!-- 历史记录 -->
-    <div class="history-box">
+    <div class="history-box" v-else>
       <div class="head">
         <span>历史记录</span>
         <van-icon name="delete"></van-icon>
@@ -28,39 +28,44 @@
 
 <script>
 export default {
-  name: 'search'
+  name: 'search',
+  data () {
+    return {
+      q: ''
+    }
+  }
 }
 </script>
 
 <style lang='less' scoped>
 .history-box {
   padding: 0 20px;
-  .head{
+  .head {
     line-height: 36px;
     color: #999;
-    .van-icon{
+    .van-icon {
       font-size: 16px;
       float: right;
-      margin-top: 10px;;
+      margin-top: 10px;
     }
   }
-  .van-cell{
+  .van-cell {
     padding: 10px 0;
   }
-  .word_btn{
-    color:#3296fa;
+  .word_btn {
+    color: #3296fa;
   }
-  .close_btn{
-    margin-top:5px;
+  .close_btn {
+    margin-top: 5px;
     color: #999;
   }
 }
-.suggest-box{
-  /deep/ .van-cell{
+.suggest-box {
+  /deep/ .van-cell {
     padding: 10px 20px;
     color: #999;
-    p{
-      span{
+    p {
+      span {
         color: red;
       }
     }
