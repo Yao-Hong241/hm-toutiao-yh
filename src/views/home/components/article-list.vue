2aa84c59613ec6976dc63b3ef842883c76644f11
YaoHong <Yao_h@sina.cn>
2020-02-04 16:52:13 +0800

    article-list实现上拉加载

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -1,8 +1,10 @@
 <template>
- <!-- 这里注意 这个div设置了滚动条 目的是 给后面做 阅读记忆 留下伏笔 -->
- <!-- 阅读记忆 => 看文章看到一半 滑到中部 去了别的页面 当你回来时 文章还在你看的位置 -->
-  <div class='scroll-wrapper'>
-      <van-list v-model="upLoading" :finished="finished" @load="onLoad"></van-list>
+  <!-- 这里注意 这个div设置了滚动条 目的是 给后面做 阅读记忆 留下伏笔 -->
+  <!-- 阅读记忆 => 看文章看到一半 滑到中部 去了别的页面 当你回来时 文章还在你看的位置 -->
+  <div class="scroll-wrapper">
+    <van-list v-model="upLoading" :finished="finished" finished-text="没有了" @load="onLoad">
+      <van-cell v-for="article in articles" :key="article" :title="article"></van-cell>
+    </van-list>
   </div>
 </template>
 
@@ -12,16 +14,28 @@ export default {
   data () {
     return {
       upLoading: false, // 是否加载数据
-      finished: false // 加载是否完成
+      finished: false, // 加载是否完成
+      articles: [] // 定义一个数组
     }
   },
   methods: {
     onLoad () {
-    // 加载方法
-      console.log('开始加载数据')
+      // 加载方法
+      setTimeout(() => {
+        if (this.articles.length === 50) {
+          // 停止追加
+          this.finished = true
+        } else {
+          let arr = Array.from(
+            Array(10),
+            (value, index) => index + this.articles.length + 1
+          )
+          this.articles.push(...arr) // 把生成的数据追加到末尾
+          this.upLoading = false // 关闭状态
+        }
+      }, 1000)
     }
   }
-
 }
 </script>
 
