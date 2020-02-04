261cc17cad2267f79930f480e3e13bb668a69646
YaoHong <Yao_h@sina.cn>
2020-02-04 18:11:25 +0800

    将channel_id传递给article_list和生成timestamp

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -41,7 +41,15 @@ export default {
       upLoading: false, // 是否加载数据
       finished: false, // 加载是否完成
       articles: [], // 定义一个数组
-      refreshSuccessText: '更新成功'
+      refreshSuccessText: '更新成功',
+      timestamp: null // 定义一个时间戳
+    }
+  },
+  props: {
+    channel_id: {
+      required: true,
+      type: Number,
+      default: null
     }
   },
   methods: {
