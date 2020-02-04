850e17800c798bab47b94a6114eb645df496c067
YaoHong <Yao_h@sina.cn>
2020-02-04 10:39:37 +0800

    使用vant

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import vant from 'vant' // 引入vant组件库
+import 'vant/lib/index.css' // 样式文件
+Vue.use(vant) // 一旦完成祖册  在任意地方都可以使用vant组件库
 Vue.config.productionTip = false
 
 new Vue({
